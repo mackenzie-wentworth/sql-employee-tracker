fd2299ebf1a636576bf5becaddc812cfe0c6e220
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-17 17:23:38 -0400

    added beginning content to server.js file to import dependencies and connect to mysql database

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 // TODO: Import module dependencies
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 
+const PORT = process.env.PORT || 3001;
+
+// Connect to SQL database
+const db = mysql.createConnection(
+  {
+    host: '127.0.0.1',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: '12345',
+    database: 'employee_DB'
+  },
+  console.log(`Connected to the employee_DB database`)
+);
 // TODO: Create command-line prompts for user input
 // Prompt options: (1)View all departments, (2)View all roles, (3)View all employees, (4)Add a department, (5)Add a role, (6)Add an employee, (7)Update an employee role
