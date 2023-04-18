6371e9d030d73811f7fdc67ec9f5be79b594f6e1
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-18 15:11:01 -0400

    updated schema.sql file to add FOREIGN KEYS and REFERENCES to tables

--- db/schema.sql
+++ db/schema.sql
@@ -15,8 +15,9 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30) NOT NULL,
-    salary DECIMAL(10.3) NOT NULL,
-    department_id INT NOT NULL
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY(department_id) REFERENCES departments(id)
 );
 
 -- Added MANAGER table to be the PRIMARY KEY for 'manager_id' in the employee table
@@ -33,5 +34,7 @@ CREATE TABLE employee (
     first_name VARCHAR (30) NOT NULL,
     last_name VARCHAR (30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NULL
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
