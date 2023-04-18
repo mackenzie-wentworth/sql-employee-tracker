b8dde7ac977cf32e88a47eab78f0d556652e2324
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-18 11:16:12 -0400

    added values to each table in seeds.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ USE employeesMgmtDB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    department_name VARCHAR (30) NOT NULL,
+    name VARCHAR (30) NOT NULL,
 );
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,27 @@
 -- TODO: Create 'INSERT' data with 'VALUES' for tables in schema.sql file
 
-INSERT INTO department (id, department_name)
-VALUES 
+INSERT INTO department (id, name)
+VALUES  (1, "Engineering"),
+        (2, "Finance"),
+        (3, "Legal"),
+        (4, "Sales")
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES 
+VALUES  (1, "Sales Lead", 100000, 4),
+        (2, "Salesperson", 80000, 4),
+        (3, "Lead Engineer", 150000, 1),
+        (4, "Software Engineer", 120000, 1),
+        (5, "Account Manager", 160000, 2),
+        (6, "Accountant", 125000, 2),
+        (7, "Legal Team Lead" 250000, 3),
+        (8, "Lawyer", 190000, 3)
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES 
+INSERT INTO employee (id, role_id, first_name, last_name, manager_id)
+VALUES  (1, 1, "John", "Doe",),
+        (2, 2, "Mike", "Chan",),
+        (3, 3, "Ashley", "Rodriguez",),
+        (4, 4, "Kevin", "Tupik",),
+        (5, 5, "Kunal", "Singh",),
+        (6, 6, "Malia", "Brown",),
+        (7, 7, "Sarah", "Lourd",),
+        (8, 8, "Tom", "Allen",)
