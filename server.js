02073974c5c838d19935bbcb27cb19dce508dfb4
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-18 13:46:03 -0400

    added prompt OPTIONS for user to navigate to and SELECT queries in server.js file

--- server.js
+++ server.js
@@ -17,5 +17,20 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employeesMgmtDB database`)
 );
-// TODO: Create command-line prompts for user input
+
+
 // Prompt options: (1)View all departments, (2)View all roles, (3)View all employees, (4)Add a department, (5)Add a role, (6)Add an employee, (7)Update an employee role
+
+const viewDeptOption = "View All Departments";
+const viewRolesOption = "View All Roles";
+const viewEmployeeOption = "View All Employees";
+const addDeptOption = "Add A Department";
+const addRoleOption = "Add A Role";
+const addEmployeeOption = "Add An Employee";
+const updateEmployeeRoleOption = "Update An Employee Role";
+const quitOption = "Quit";
+
+// Queries 
+const viewDeptQuery = "SELECT * FROM department";
+const viewRolesQuery = "SELECT * FROM role";
+const viewEmployeeQuery = "SELECT * FROM employee";
