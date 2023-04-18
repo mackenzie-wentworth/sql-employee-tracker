fe325f842aebd80de3ca2ab2cfcb9db467a53edc
mackenzie-wentworth <mwentworth28@gmail.com>
2023-04-18 09:45:13 -0400

    updated database name in server.js file and added content to schema.sql file to create tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,30 @@
 -- TODO: Create 'ALL departments' table (INCLUDES: id, name)
+DROP DATABASE IF EXISTS employeesMgmtDB;
+
+CREATE DATABASE employeesMgmtDB;
+
+USE employeesMgmtDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR (30) NOT NULL,
+);
+
 
 -- TODO: Create 'ALL roles' table (INCLUDES: id, title, salary, department_id)
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL(10.3) NULL,
+    department_id INT NOT NULL
+);
+
 
 -- TODO: Create 'ALL employees' table (INCLUDES: id, first_name, last_name, role_id, manager_id)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL
+);
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const db = mysql.createConnection(
     user: 'root',
     // TODO: Add MySQL password here
     password: '12345',
-    database: 'employee_DB'
+    database: 'employeesMgmtDB'
   },
-  console.log(`Connected to the employee_DB database`)
+  console.log(`Connected to the employeesMgmtDB database`)
 );
 // TODO: Create command-line prompts for user input
 // Prompt options: (1)View all departments, (2)View all roles, (3)View all employees, (4)Add a department, (5)Add a role, (6)Add an employee, (7)Update an employee role
